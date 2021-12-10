10d27e7d5c32521a482df18c1f3a81228cb64606
SUBHRASANKHA <imsubhranasaastronaut@gmail.com>
2021-12-10 23:24:54 +0530

    deployment

--- world_weather_website/server.js
+++ world_weather_website/server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/world_weather_website'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/world_weather_website/index.html'));});
+res.sendFile(path.join(__dirname+'/dist/world_weather_website/index.html'));});
 app.listen(process.env.PORT || 8080);
